44038de64bacb3b94ac7bcf49700410a8f9c3396
cra2ypierr0t <uchiyama.kazuhide@gmail.com>
2020-04-17 09:54:54 +0900

    Changes to be committed: 	modified:   ALU.v 	new file:   ALU_decoder.v

--- ALU.v
+++ ALU.v
@@ -1,18 +1,41 @@
 module ALU(
-    input   wire            alu_ctrl,
+    input   wire    [3:0]   alu_ctrl,
     input   wire    [31:0]  alu_data_1,
     input   wire    [31:0]  alu_data_2,
     output  wire    [31:0]  alu_out
 );
 
+parameter ALU_ADD   = 4'h0;
+parameter ALU_SUB   = 4'h1;
+parameter ALU_XOR   = 4'h2;
+parameter ALU_OR    = 4'h3;
+parameter ALU_AND   = 4'h4;
+parameter ALU_SLL   = 4'h5;
+parameter ALU_SRL   = 4'h6;
+parameter ALU_SRA   = 4'h7;
+parameter ALU_SLT   = 4'h8;
+parameter ALU_SLTU  = 4'h9;
+
+
+    assign alu_out = alu_exec(alu_ctrl, alu_data_1, alu_data_2);
+
     function [31:0] alu_exec(
-        input           alu_ctrl,
+        input   [3:0]   alu_ctrl,
         input   [31:0]  alu_data_1,
-        input   [31:0]  alu_data_2,
-        output  [31:0]  alu_out
+        input   [31:0]  alu_data_2 
     );
         begin
             case(alu_ctrl)
+                ALU_ADD:    alu_exec = alu_data_1 + alu_data_2;
+                ALU_SUB:    alu_exec = alu_data_1 - alu_data_2;
+                ALU_XOR:    alu_exec = alu_data_1 ^ alu_data_2;
+                ALU_OR:     alu_exec = alu_data_1 | alu_data_2;
+                ALU_AND:    alu_exec = alu_data_1 & alu_data_2;
+                ALU_SLL:    alu_exec = alu_data_1 << alu_data_2;
+                ALU_SRL:    alu_exec = alu_data_1 >> alu_data_2;
+                ALU_SRA:    alu_exec = $signed(alu_data_1) >>> $signed(alu_data_2);
+                ALU_SLT:    alu_exec = alu_data_1 < alu_data_2;
+                ALU_SLTU:   alu_exec = $signed(alu_data_1) < $signed(alu_data_2);
             endcase
         end
     endfunction
---
+++ ALU_decoder.v
@@ -0,0 +1,47 @@
+module ALU_decoder(
+    input   wire    [2:0]   funct3,
+    input   wire    [6:0]   funct7,
+    output  wire    [3:0]   alu_ctrl 
+);
+
+parameter ALU_ADD   = 4'h0;
+parameter ALU_SUB   = 4'h1;
+parameter ALU_XOR   = 4'h2;
+parameter ALU_OR    = 4'h3;
+parameter ALU_AND   = 4'h4;
+parameter ALU_SLL   = 4'h5;
+parameter ALU_SRL   = 4'h6;
+parameter ALU_SRA   = 4'h7;
+parameter ALU_SLT   = 4'h8;
+parameter ALU_SLTU  = 4'h9;
+
+    assign alu_ctrl = alu_dec(funct3, funct7);
+
+    function [3:0] alu_dec(
+        input   [2:0]   funct3,
+        input   [6:0]   funct7
+    );
+        begin
+            case(funct3)
+                3'h0: begin
+                    case(funct7)
+                        7'h00: alu_dec = ALU_ADD;
+                        7'h20: alu_dec = ALU_SUB;
+                    endcase
+                end
+                3'h4: alu_dec = ALU_XOR;
+                3'h6: alu_dec = ALU_OR;
+                3'h7: alu_dec = ALU_AND;
+                3'h1: alu_dec = ALU_SLL;
+                3'h5: begin
+                    case(funct7)
+                        7'h00: alu_dec = ALU_SRL;
+                        7'h20: alu_dec = ALU_SRA;
+                    endcase
+                end
+                3'h2: alu_dec = ALU_SLT;
+                3'h3: alu_dec = ALU_SLTU;
+            endcase
+        end
+    endfunction
+endmodule
